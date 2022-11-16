4289e2c28bf64ccc10f6715f533cb410ec9e76b7
Xiaoguang Wang <guangxiaozhi@hotmail.com>
2022-11-16 15:13:16 -0800

    Phase 1

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,7 +6,23 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+const string = "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery";
+const body = string.split('&').map((keyValuePair) => keyValuePair.split('='))
+.map(([key,value]) =>[key, value.replace(/\+/g, ' ')])
+.map(([key, value])=>[key, decodeURIComponent(value)])
+.reduce((acc,[key, value])=>{
+  acc[key] = value;
+  return acc;
+},{});
 
+console.log("beforeproducts");
+fetch('/products',{
+  method:"POST",
+  headers:{
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  body: string,
+})
 
 
 /* ============================== Phase 2 ============================== */
